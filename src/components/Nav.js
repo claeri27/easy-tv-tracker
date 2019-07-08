867f52e6edda1d7869977d74f036d22ea27496e2
Christopher Laeri <claeri27@gmail.com>
2019-07-08 14:22:31 -0400

    Removed material ui and remade nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,49 +1,52 @@
 import React, { useState } from 'react'
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
-import { makeStyles } from '@material-ui/core/styles'
+import styled, { createGlobalStyle, ThemeProvider } from 'styled-components'
 import lightTheme from "../themes/light"
 import darkTheme from "../themes/dark"
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
-import Button from '@material-ui/core/Button'
 
 const GlobalStyle = createGlobalStyle`
   html {background: ${props => props.theme.colors.background}}
 `;
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}))
+const NavContainer = styled.div`
+  display: flex;
+  position: static;
+  height: 60px;
+  background: blue;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const TitleContainer = styled.h1`
+  margin-left: 10px;
+`;
+
+const ButtonsContainer = styled.div`
+  margin-right: 10px;
+`;
+
+const Button = styled.button`
+  padding: 5px;
+  cursor: pointer;
+`;
 
 const Nav = () => {
-  const classes = useStyles()
   const stored = localStorage.getItem("lightMode")
   const [lightMode, setLightMode] = useState(stored === "true" ? true : false);
 
   return <ThemeProvider theme={lightMode === false ? darkTheme : lightTheme}>
-    <div className={classes.root}>
-      <GlobalStyle/>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" className={classes.title}>Easy TV Tracker</Typography>
-          <Button color="inherit" onClick={() => {
-            setLightMode(!lightMode);
-            localStorage.setItem("lightMode", !lightMode);
-          }}>Theme</Button>
-          <Button color="inherit">Login</Button>
-          <Button color="inherit">Register</Button>
-        </Toolbar>
-      </AppBar>
-    </div>
+    <NavContainer>
+      <GlobalStyle />
+      <TitleContainer>EzTvT</TitleContainer>
+      <ButtonsContainer>
+        <Button onClick={() => {
+          setLightMode(!lightMode);
+          localStorage.setItem("lightMode", !lightMode);
+        }}>Theme</Button>
+        <Button>LOGIN</Button>
+        <Button>REGISTER</Button>
+      </ButtonsContainer>
+      </NavContainer>
   </ThemeProvider>
 }
 
