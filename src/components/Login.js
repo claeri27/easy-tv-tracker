63a14a0ee4171e2f9a19e13ded810efc1b9259fc
Christopher Laeri <claeri27@gmail.com>
2019-07-08 19:18:35 -0400

    Created basic login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,20 +7,64 @@ const LoginContainer = styled.div`
   align-items: center;
   justify-content: center;
   background: grey;
-  min-height: 500px;
-  font-size: 3em;
+  min-height: 300px;
+`;
+
+const FormContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+const UsernameContainer = styled.label`
+`;
+
+const UsernameInput = styled.input`
+  padding: 6px;
+  margin: 2px;
+`;
+
+const PasswordContainer = styled.label`
+`;
+
+const PasswordInput = styled.input`
+  padding: 6px;
+  margin: 2px;
+`;
+
+const SubmitButton = styled.button`
+  padding-top: 10px;
+  padding-bottom: 10px;
+  cursor: pointer;
+  &:hover {
+    background: springgreen;
+  }
+  &:focus {
+    outline: none;
+  }
 `;
 
 const BASE_URL = 'http://localhost:3000'
 
 const Login = () => {
-  // const [username, setUsername] = useState('');
-  // const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   // const [token, setToken] = useState('');
   // const [logged, setLogged] = useState(false);
 
   return <LoginContainer>
-    LOGIN
+    <FormContainer>
+      <UsernameContainer>
+        Username: {` `}
+        <UsernameInput type="text" name="username" value={username}/>
+      </UsernameContainer>
+      <br></br>
+      <PasswordContainer>
+        Password: {` `}
+        <PasswordInput type="text" name="password" value={password}/>
+      </PasswordContainer>
+      <br></br>
+      <SubmitButton type="submit">LOGIN</SubmitButton>
+    </FormContainer>
   </LoginContainer>
 }
 
